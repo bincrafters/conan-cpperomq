f869660d4f47f3cb3f73bdb84bc15a17b91bd437
SSE4 <tomskside@gmail.com>
2018-02-20 00:34:12 +0700

    - initial version

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,27 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <CpperoMQ/All.hpp>
 
 int main()
 {
-    std::cout << "Bincrafters\n";
+    using namespace CpperoMQ;
+
+    Context context;
+
+    PullSocket pull_socket(context.createPullSocket());
+    pull_socket.bind("inproc://example");
+
+    PushSocket push_socket(context.createPushSocket());
+    push_socket.connect("inproc://example");
+
+    push_socket.send(OutgoingMessage("Hello, CpperoMQ!"));
+
+    IncomingMessage msg;
+    pull_socket.receive(msg);
+
+    std::string str(msg.charData(), msg.size());
+    std::cout << str << std::endl;
+
     return EXIT_SUCCESS;
 }
