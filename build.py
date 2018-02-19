4fde8719135f984c46dc5590240262c0c5c8d58f
SSE4 <tomskside@gmail.com>
2018-02-20 00:34:52 +0700

    - header-only configuration

--- build.py
+++ build.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-
-from bincrafters import build_template_default
+from bincrafters import build_template_header_only
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder()
+    builder = build_template_header_only.get_builder()
 
     builder.run()
